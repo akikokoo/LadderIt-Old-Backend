0225b711bf138c696ece5c4bd5b95f3facc76041
Akif Emre Reis <akifemre611@hotmail.com>
2023-10-10 12:48:21 +0300

    secret key geçici olarak settings.py a eklendi

--- configuration/settings.py
+++ configuration/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY ="django-insecure-z-^)akz0-d^noh*(glg@9y6$d23!)wrkkhp)-i$@(wtgsqcrpm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
